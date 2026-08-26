ddb8b81d0e3400cab7971463dc421a0b0b24c3e4
Jonathon Wickes <jonathon@wickes.ca>
2026-08-26 15:11:25 -0400

    Add app icon (embedded in exe), brand resources

--- build.rs
+++ build.rs
@@ -1,3 +1,10 @@
 fn main() {
     println!("cargo:rustc-link-lib=advapi32");
+
+    #[cfg(target_os = "windows")]
+    {
+        let mut res = winresource::WindowsResource::new();
+        res.set_icon("resources/icon.ico");
+        res.compile().expect("Failed to compile Windows resources");
+    }
 }
